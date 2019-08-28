14d6195801cca21daa01028c9feeb6cdf5e29abe
Lucas Dias <lucasdiasrodrigues@hotmail.com>
2019-08-28 11:44:37 -0300

    Finished form template for goals and starting button creation

--- js/select-options.js
+++ js/select-options.js
@@ -1,5 +1,7 @@
-
 let diretoriaOptions = ['Selecione', 'Compras e Logística', 'Finanças', 'Desenvolvimento', 'Operações Outback', 'TI & Digital', 'Presidência', 'Operações Abbraccio', 'Experiência do Cliente', 'Marketing', 'Gestão de Pessoas', 'Novos Negócios', 'Flemings'];
+let pesoOptions = [10, 20, 30, 40, 50, 60, 70, 80];
+let prioridadeOptions = ['Selecione'];
+let prazoOptions = ['Selecione'];
 
 function selectOptions(selectId, arrayOptions) {
   let select = document.getElementById(selectId);
@@ -13,3 +15,6 @@ function selectOptions(selectId, arrayOptions) {
 }
 
 selectOptions('diretoria', diretoriaOptions);
+selectOptions('peso', pesoOptions);
+selectOptions('prior-estrat', prioridadeOptions);
+selectOptions('prazo', prazoOptions);
